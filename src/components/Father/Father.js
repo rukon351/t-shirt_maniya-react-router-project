5e3e1616c6cce495b2b1d8dd4f1461a353ace77e
Rukonuzzaman Shiem <rukonuzzaman.shiem.351@gmail.com>
2022-11-16 19:28:00 +0600

    added special person gift

--- src/components/Father/Father.js
+++ src/components/Father/Father.js
@@ -3,12 +3,12 @@ import MySelf from '../MySelf/MySelf';
 import Brother from '../Brother/Brother';
 import Sister from '../Sister/Sister';
 
-const Father = ({ house }) => (
+const Father = ({ house, ornament }) => (
     <div>
         <h5>Father!</h5>
         <p>House: {house}</p>
         <div style={{ display: 'flex' }}>
-            <MySelf house={house}></MySelf>
+            <MySelf house={house} ornament={ornament}></MySelf>
             <Brother house={house}></Brother>
             <Sister house={house}></Sister>
         </div>
--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Uncle from '../Uncle/Uncle';
 import Father from '../Father/Father';
 import Aunty from '../Aunty/Aunty';
@@ -6,12 +6,24 @@ import './Grandpa.css';
 
 
 const Grandpa = () => {
-    const house = 7;
+    const [house, setHouse] = useState(1);
+
+    const ornament = 'Diamond Ring';
+
+    const handleBuyAHouse = () => {
+        const newHouseCount = house + 1;
+        setHouse(newHouseCount);
+    };
+
     return (
-        <div className='grandpa' style={{ display: 'flex' }}>
-            <Father house={house}></Father>
-            <Uncle house={house}></Uncle>
-            <Aunty house={house}></Aunty>
+        <div className='grandpa'>
+            <h4>Grandpaa</h4>
+            <p>House: {house} <button onClick={handleBuyAHouse}>Buy a House</button></p>
+            <section style={{ display: 'flex' }}>
+                <Father house={house} ornament={ornament}></Father>
+                <Uncle house={house}></Uncle>
+                <Aunty house={house}></Aunty>
+            </section>
         </div>
     );
 };
