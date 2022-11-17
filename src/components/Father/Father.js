1d7030572073c6fd8dc86f4bc9be00e7b89351d9
Rukonuzzaman Shiem <rukonuzzaman.shiem.351@gmail.com>
2022-11-17 14:34:04 +0600

    added grandpa special gift for aunty & special person

--- src/components/Father/Father.js
+++ src/components/Father/Father.js
@@ -3,12 +3,12 @@ import MySelf from '../MySelf/MySelf';
 import Brother from '../Brother/Brother';
 import Sister from '../Sister/Sister';
 
-const Father = ({ house, ornament }) => (
+const Father = ({ house }) => (
     <div>
         <h5>Father!</h5>
         <p>House: {house}</p>
         <div style={{ display: 'flex' }}>
-            <MySelf house={house} ornament={ornament}></MySelf>
+            <MySelf house={house}></MySelf>
             <Brother house={house}></Brother>
             <Sister house={house}></Sister>
         </div>
--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import Uncle from '../Uncle/Uncle';
 import Father from '../Father/Father';
 import Aunty from '../Aunty/Aunty';
 import './Grandpa.css';
 
+export const RingContext = createContext('Ring');
+
 
 const Grandpa = () => {
     const [house, setHouse] = useState(1);
 
-    const ornament = 'Diamond Ring';
+    const ornament = 'Kada Matir Ring';
 
     const handleBuyAHouse = () => {
         const newHouseCount = house + 1;
@@ -16,15 +18,17 @@ const Grandpa = () => {
     };
 
     return (
-        <div className='grandpa'>
-            <h4>Grandpaa</h4>
-            <p>House: {house} <button onClick={handleBuyAHouse}>Buy a House</button></p>
-            <section style={{ display: 'flex' }}>
-                <Father house={house} ornament={ornament}></Father>
-                <Uncle house={house}></Uncle>
-                <Aunty house={house}></Aunty>
-            </section>
-        </div>
+        <RingContext.Provider value={ornament}>
+            <div className='grandpa'>
+                <h4>Grandpaa</h4>
+                <p>House: {house} <button onClick={handleBuyAHouse}>Buy a House</button></p>
+                <section style={{ display: 'flex' }}>
+                    <Father house={house}></Father>
+                    <Uncle house={house}></Uncle>
+                    <Aunty house={house}></Aunty>
+                </section>
+            </div>
+        </RingContext.Provider>
     );
 };
 
